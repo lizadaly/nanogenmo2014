ea5762949969268cb70da083d2d3aabcca5618d2
Liza Daly <lizadaly@gmail.com>
2014-10-27 21:25:16 -0400

    Get 5 photos per page

--- flickr.py
+++ flickr.py
@@ -8,15 +8,19 @@ import flickrapi
 from secret import FLICKR_KEY
 
 FLICKR_USER_ID = '126377022@N07'  # The Internet Archive's Flickr ID
+MAX_PHOTOS_PER_PAGE = 5
 
 def flickr_search():
     flickr = flickrapi.FlickrAPI(FLICKR_KEY, format='etree')
-    for photo in flickr.walk(user_id=FLICKR_USER_ID,
-                             text='bird',
-                             tag_mode='all',
-                             tags='bookcentury1500',
-                             sort='relevance'):
+    photos = flickr.walk(user_id=FLICKR_USER_ID,
+                         per_page=5,
+                         text='bird',
+                         tag_mode='all',
+                         tags='bookcentury1800',
+                         sort='relevance')
 
+    count = 0
+    for photo in photos:
         ## https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
         img = "https://farm{farm_id}.staticflickr.com/{server_id}/{photo_id}_{secret}.jpg".format(
@@ -24,9 +28,11 @@ def flickr_search():
             server_id=photo.get('server'),
             photo_id=photo.get('id'),
             secret=photo.get('secret'))
-
         print '<img src="{}">'.format(img)
-
+        count += 1
+        if count > MAX_PHOTOS_PER_PAGE:
+            break
+    
 if __name__ == '__main__':
     flickr_search()
     
