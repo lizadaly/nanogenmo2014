9ad61cfb8365e41f415b86de7dc8b500739d50c8
Liza Daly <lizadaly@gmail.com>
2014-10-27 21:16:04 -0400

    Print a range of photos

--- flickr.py
+++ flickr.py
@@ -13,9 +13,19 @@ def flickr_search():
     flickr = flickrapi.FlickrAPI(FLICKR_KEY, format='etree')
     for photo in flickr.walk(user_id=FLICKR_USER_ID,
                              text='bird',
-                             sort='relevance',
-                         ):
-        print photo.get('title')
+                             tag_mode='all',
+                             tags='bookcentury1500',
+                             sort='relevance'):
+
+        ## https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+        img = "https://farm{farm_id}.staticflickr.com/{server_id}/{photo_id}_{secret}.jpg".format(
+            farm_id=photo.get('farm'),
+            server_id=photo.get('server'),
+            photo_id=photo.get('id'),
+            secret=photo.get('secret'))
+
+        print '<img src="{}">'.format(img)
 
 if __name__ == '__main__':
     flickr_search()
