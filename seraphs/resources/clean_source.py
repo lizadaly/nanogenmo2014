4952702990a3f94d398e66827be02d65eaf8ac95
Liza Daly <lizadaly@gmail.com>
2014-11-02 16:17:06 -0500

    Generate a more accurate word list

--- seraphs/resources/clean_source.py
+++ seraphs/resources/clean_source.py
@@ -6,3 +6,23 @@
 
 # Clean the EVA source to just a set of words, one word per row
 
+import re
+
+marker = re.compile('<[^>]+>')
+
+filename = 'text16e6.evt'
+
+out = open('words.txt', 'w')
+
+for line in open(filename):
+    line = line.strip()
+    if line.startswith('#'):
+        continue
+    if '%' in line or '{' in line or '!!' in line:
+        continue
+    line = marker.sub('', line)
+    line = line.strip()
+    line = line.replace('-', '')
+    line = line.replace('=', '')    
+    for word in line.split('.'):
+        out.write(word + "\n")
